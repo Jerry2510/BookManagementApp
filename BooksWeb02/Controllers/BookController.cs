c27c0dbf01cf6278afeb612938ed0b92de200db8
Sachin <jerryon1027@gmail.com>
2023-08-23 14:08:03 +0530

    Author,addauthor,book,addbook working

---
+++ BooksWeb02/Controllers/BookController.cs
@@ -0,0 +1,155 @@
+﻿using ConceptArchitect.BookManagement;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace BooksWeb02.Controllers
+{
+    public class BookController : Controller
+    {
+        IBookService bookService;
+
+
+
+        public BookController(IBookService books)
+        {
+            this.bookService = books;
+        }
+
+
+
+        public async Task<ViewResult> Index()
+        {
+            var books = await bookService.GetAllBooks();
+
+
+
+            return View(books);
+        }
+
+
+
+        public async Task<ViewResult> Details(string id)
+        {
+            var book = await bookService.GetBookById(id);
+
+
+
+            return View(book);
+        }
+
+        [HttpGet]
+        public ViewResult Delete()
+        {
+            return View();
+        }
+
+
+
+
+
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            return View(new Book());
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> Add(Book book)
+        {
+            await bookService.AddBook(book);
+
+
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task Delete(string bookId)
+        {
+            await bookService.DeleteBook(bookId);
+            Index();
+            RedirectToAction("Index");
+
+
+
+        }
+        [HttpGet]
+        public async Task<ViewResult> Edit(string id)
+        {
+            var author = await bookService.GetBookById(id);
+            return View(author);
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Book book)
+        {
+            await bookService.UpdateBook(book);
+
+
+
+            return RedirectToAction("Index");
+        }
+
+        
+
+
+
+        public async Task<ActionResult> SaveV2(Book book)
+        {
+            await bookService.AddBook(book);
+
+
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+        public Book SaveV1(string id, string title, string description, string authorId, string photourl)
+        {
+            var book = new Book()
+            {
+                Id = id,
+                Title = title,
+                Description = description,
+                AuthorId = authorId,
+                CoverPhoto = photourl
+            };
+
+
+
+            return book;
+        }
+
+
+
+        public Book SaveV0()
+        {
+            var book = new Book()
+            {
+                Id = Request.Form["id"],
+                Title = Request.Form["title"],
+                Description = Request.Form["description"],
+                AuthorId = Request.Form["authorId"],
+                CoverPhoto = Request.Form["photourl"]
+            };
+
+
+
+            return book;
+        }
+
+
+
+
+
+    }
+}
---
+++ ConceptArchitect.BookManagement.Repositories.Ado/AdoBookRepository.cs
@@ -0,0 +1,94 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConceptArchitect.Data;
+using ConceptArchitect.Utils;
+using System.Data;
+
+namespace ConceptArchitect.BookManagement.Repositories.Ado
+{
+
+
+    public class AdoBookRepository : IRepository<Book, string>
+    {
+        DbManager db;
+
+        public AdoBookRepository(DbManager db)
+        {
+            this.db = db;
+        }
+
+        public async Task<Book> Add(Book book)
+        {
+            var query = $"insert into books(id,title,description,author_id,cover_photo) " +
+                              $"values('{book.Id}','{book.Title}','{book.Description}','{book.AuthorId}','{book.CoverPhoto}')";
+
+            await db.ExecuteUpdateAsync(query);
+
+            return book;
+        }
+
+        public async Task Delete(string id)
+        {
+            await db.ExecuteUpdateAsync($"delete from books where id='{id}'");
+        }
+
+        private Book BookExtractor(IDataReader reader)
+        {
+            Book book = new Book();
+
+            book.Id = reader["id"].ToString();
+              book.Title = reader["title"].ToString();
+                book.Description = reader["description"].ToString();
+                book.AuthorId = reader["author_id"].ToString();
+                book.CoverPhoto = reader["cover_photo"].ToString();
+            return book;
+            
+        }
+
+        public async Task<List<Book>> GetAll()
+        {
+            return await db.QueryAsync("select * from books", BookExtractor);
+        }
+
+        public async Task<List<Book>> GetAll(Func<Book, bool> predicate)
+        {
+            var books = await GetAll();
+
+            return (from book in books
+                    where predicate(book)
+                    select book
+                   ).ToList();
+        }
+
+        public async Task<Book> GetById(string id)
+        {
+            return await db.QueryOneAsync($"Select * from book where id='{id}'",BookExtractor);
+        }
+
+        public async Task<Book> Update(Book entity, Action<Book, Book> mergeOldNew)
+        {
+            var oldBook = await GetById(entity.Id);
+            if (oldBook != null)
+            {
+                mergeOldNew(oldBook, entity);
+                var query = $"update books set " +
+                            $"id='{oldBook.Title}', " +
+                            $"description='{oldBook.Description}', " +
+                            $"author_id='{oldBook.AuthorId}, " +
+                            $"cover_photo='{oldBook.CoverPhoto} " +
+                            $"where id='{oldBook.Id}'";
+
+                await db.ExecuteUpdateAsync(query);
+            }
+
+            return entity;
+
+
+
+        }
+    }
+}
